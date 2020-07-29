a55645bd2df4a3255be2db966f34e2cd3b29e676
rawdom <rawdom@rawdom.com>
2020-07-30 03:57:21 +1000

    The real INIT

---
+++ Source/CardGameTest/CardGameTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CardGameTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDGAMETEST_API ACardGameTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
